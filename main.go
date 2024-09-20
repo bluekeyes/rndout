24db76e73d00ec353512a28a776c9cb5facf58db
Billy Keyes <commits@billykeyes.dev>
2024-09-20 14:52:41 -0700

    Add mode flag, implement ramp mode
    This produces a deterministic output rate (when skips are disabled),
    which is less realistic, but more useful for consistent tests.

--- main.go
+++ main.go
@@ -15,22 +15,32 @@ const (
 	alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.- "
 )
 
+const (
+	LogisticMode = "logistic"
+	RampMode     = "ramp"
+)
+
 var opts struct {
 	peakRate string
-	scale    int
+	mode     string
 	skips    int
 	skipProb float64
 
-	duration time.Duration
-	stepSize time.Duration
-	sliceLen int
-
+	duration  time.Duration
+	stepSize  time.Duration
+	sliceLen  int
 	blockSize int
+
+	// logistic flags
+	scale int
+
+	// ramp flags
+	rampDuration time.Duration
 }
 
 func init() {
 	flag.StringVar(&opts.peakRate, "rate", "128", "peak character rate in chars/s")
-	flag.IntVar(&opts.scale, "scale", 25, "scale factor for the output logistic distribution")
+	flag.StringVar(&opts.mode, "mode", LogisticMode, "the operation mode, one of 'logistic' or 'ramp'")
 
 	flag.IntVar(&opts.skips, "skips", 2, "expected number of time steps with no output per slice")
 	flag.Float64Var(&opts.skipProb, "skip-probability", 0, "probability that a given slice will contain skips")
@@ -38,12 +48,19 @@ func init() {
 	flag.DurationVar(&opts.duration, "duration", 60*time.Second, "duration")
 	flag.DurationVar(&opts.stepSize, "step-size", 250*time.Millisecond, "length of each time step")
 	flag.IntVar(&opts.sliceLen, "slice-length", 16, "number of time steps per slice")
-
 	flag.IntVar(&opts.blockSize, "block-size", 4096, "maximum number of characters printed in one line/operation")
+
+	// logistic flags
+	flag.IntVar(&opts.scale, "scale", 25, "scale factor for the output distribution; only used with -mode=logistic")
+
+	// ramp flags
+	flag.DurationVar(&opts.rampDuration, "ramp-duration", 10*time.Second, "time taken to reach the peak rate; only used with -mode=ramp")
 }
 
 func main() {
 	flag.Parse()
+
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
 	rate, err := parseRate(opts.peakRate)
 	if err != nil {
@@ -59,11 +76,23 @@ func main() {
 		die("invalid skip probability: must be in [0.0, 1.0]")
 	}
 
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	out := NewRandomOutput(r, 32, opts.blockSize)
-
 	charsPerStep := float64(rate) * opts.stepSize.Seconds()
-	peakStep := r.Intn(int(opts.duration / opts.stepSize))
+
+	var shaper RateShaper
+	switch opts.mode {
+	case LogisticMode:
+		peakStep := r.Intn(int(opts.duration / opts.stepSize))
+		shaper = LogisticShaper{Mu: peakStep, Scale: opts.scale}
+
+	case RampMode:
+		peakStep := int(opts.rampDuration / opts.stepSize)
+		shaper = RampShaper{PeakStep: peakStep}
+
+	default:
+		die("invalid mode: must be one of 'logistic' or 'ramp'")
+	}
+
+	out := NewRandomOutput(r, 32, opts.blockSize)
 
 	end := time.After(opts.duration)
 	steps := time.Tick(opts.stepSize)
@@ -78,7 +107,7 @@ func main() {
 		select {
 		case <-steps:
 			if sliceIdx < opts.sliceLen-skips {
-				n := int(charsPerStep * rateFraction(peakStep, opts.scale, step))
+				n := int(charsPerStep * shaper.Fraction(step))
 				out.WriteN(os.Stdout, n)
 			}
 		case <-end:
@@ -110,12 +139,34 @@ func sampleSkips(r *rand.Rand, skips int, skipProb float64) int {
 	}
 }
 
-func rateFraction(mu, s, x int) float64 {
+// RateShaper shapes how the output scales by returning a multiple between 0.0
+// and 1.0 of the peak rate for each step.
+type RateShaper interface {
+	Fraction(step int) float64
+}
+
+type LogisticShaper struct {
+	Mu    int
+	Scale int
+}
+
+func (s LogisticShaper) Fraction(step int) float64 {
 	// https://en.wikipedia.org/wiki/Logistic_distribution
 	// Scaled to fit in [0.0, 1.0]
-	a := math.Exp(-float64(x-mu) / float64(s))
-	b := float64(s) * (1 + a) * (1 + a)
-	return float64(4*s) * (a / b)
+	a := math.Exp(-float64(step-s.Mu) / float64(s.Scale))
+	b := float64(s.Scale) * (1 + a) * (1 + a)
+	return float64(4*s.Scale) * (a / b)
+}
+
+type RampShaper struct {
+	PeakStep int
+}
+
+func (s RampShaper) Fraction(step int) float64 {
+	if step < s.PeakStep {
+		return float64(step) / float64(s.PeakStep)
+	}
+	return 1.0
 }
 
 type RandomOutput struct {
